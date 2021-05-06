567c3f26799ed3d0effa49c1a2bc04b14899b714
nbargers <32649870+nbargers@users.noreply.github.com>
2021-05-05 23:33:04 -0400

    start of Oauth fix for github

--- src/Client/Containers/Login.js
+++ src/Client/Containers/Login.js
@@ -1,16 +1,56 @@
 import React from 'react';
 import { Button, Text, View, StyleSheet, Image ,TouchableOpacity, Alert} from 'react-native';
+import dotenv from 'dotenv';
+
 
 export default ({history}) => {
 
-  const onSubmit = (event) => {
-    fetch('/api/login', {
-      method: 'GET',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(),
-    })
-    .then((res)=>showAlert())
-    .catch((err)=>(err))
+  
+  const clientID = '72e3b30caf0e91e8a764';
+  const url = `https://github.com/login/oauth/authorize?client_id=${clientID}&scope=repo`
+
+  const onSubmit = async (event) => {
+    // dotenv.config();
+    // console.log('clientID', clientID)
+    // console.log('url', url)
+    // fetch(url, {
+    //   mode: 'no-cors',
+    //   method: 'GET',
+    //   headers: {'Content-Type': 'text/html'},
+    //   // body: JSON.stringify(),
+    // })
+    // .then((res)=> (console.log('res', res)))
+    // .catch((err)=>(err))
+
+    const res = await fetch(url, {
+      mode: 'no-cors',
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+    return res.json();
+  };
+
+ 
+
+
+
+  const OpenURLButton = ({ url, children }) => {
+    const handlePress = useCallback(async () => {
+      // Checking if the link is supported for links with custom URL scheme.
+      const supported = await Linking.canOpenURL(url);
+  
+      if (supported) {
+        // Opening the link with some app, if the URL scheme is "http" the web link should be opened
+        // by some browser in the mobile
+        await Linking.openURL(url);
+      } else {
+        Alert.alert(`Don't know how to open this URL: ${url}`);
+      }
+    }, [url]);
+  
   };
 
 
@@ -42,7 +82,7 @@ export default ({history}) => {
        <Text>{'\n'}</Text>
         This is your teams shop to work with fixing those flats and bumps in your code. Work collaboratively and keep track of all present issues in your repositories to stay ahead of the curve. 
       </Text>
-      <TouchableOpacity style={styles.loginButton} activeOpacity={0.5} onPress={showAlert}>
+      <TouchableOpacity style={styles.loginButton} activeOpacity={0.5} onPress={onSubmit}>
         <Image 
           source={require('../github-icon.png')} 
           style={styles.ImageIconStyle} 
