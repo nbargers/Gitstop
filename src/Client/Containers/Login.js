20dee372377b089b04d47eff30118f59b7565c12
nbargers <32649870+nbargers@users.noreply.github.com>
2021-05-06 20:55:40 -0400

    Github Oauth Fix

--- src/Client/Containers/Login.js
+++ src/Client/Containers/Login.js
@@ -1,57 +1,52 @@
 import React from 'react';
 import { Button, Text, View, StyleSheet, Image ,TouchableOpacity, Alert} from 'react-native';
-import dotenv from 'dotenv';
+import * as WebBrowser from 'expo-web-browser';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+// require('dotenv').config()
+
+WebBrowser.maybeCompleteAuthSession();
+
+const clientID = '72e3b30caf0e91e8a764';
+const clientSecret = 'a3c8547c9983f3abc6ac2e3878369a89b94a0e5c';
+
+// Endpoint
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: `https://github.com/login/oauth/authorize?client_id=${clientID}&scope=repo`,
+};
 
 
 export default ({history}) => {
 
-  
-  const clientID = '72e3b30caf0e91e8a764';
-  const url = `https://github.com/login/oauth/authorize?client_id=${clientID}&scope=repo`
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: clientID,
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'http://localhost:3000/login'
+        }),
+    },
+    discovery
+  );
 
-  const onSubmit = async (event) => {
-    // dotenv.config();
-    // console.log('clientID', clientID)
-    // console.log('url', url)
-    // fetch(url, {
-    //   mode: 'no-cors',
-    //   method: 'GET',
-    //   headers: {'Content-Type': 'text/html'},
-    //   // body: JSON.stringify(),
-    // })
-    // .then((res)=> (console.log('res', res)))
-    // .catch((err)=>(err))
-
-    const res = await fetch(url, {
-      mode: 'no-cors',
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    });
-    return res.json();
-  };
-
- 
-
-
-
-  const OpenURLButton = ({ url, children }) => {
-    const handlePress = useCallback(async () => {
-      // Checking if the link is supported for links with custom URL scheme.
-      const supported = await Linking.canOpenURL(url);
-  
-      if (supported) {
-        // Opening the link with some app, if the URL scheme is "http" the web link should be opened
-        // by some browser in the mobile
-        await Linking.openURL(url);
-      } else {
-        Alert.alert(`Don't know how to open this URL: ${url}`);
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
       }
-    }, [url]);
-  
-  };
+  }, [response]);
+
+
+
+  // const onSubmit = (event) => {
+  //   fetch('/api/login', {
+  //     method: 'GET',
+  //     headers: {'Content-Type': 'application/json'},
+  //     body: JSON.stringify(),
+  //   })
+  //   .then((res)=>showAlert())
+  //   .catch((err)=>(err))
+  // };
 
 
   const showAlert = () =>
@@ -82,7 +77,7 @@ export default ({history}) => {
        <Text>{'\n'}</Text>
         This is your teams shop to work with fixing those flats and bumps in your code. Work collaboratively and keep track of all present issues in your repositories to stay ahead of the curve. 
       </Text>
-      <TouchableOpacity style={styles.loginButton} activeOpacity={0.5} onPress={onSubmit}>
+      <TouchableOpacity style={styles.loginButton} activeOpacity={0.5} onPress={showAlert}>
         <Image 
           source={require('../github-icon.png')} 
           style={styles.ImageIconStyle} 
@@ -90,6 +85,14 @@ export default ({history}) => {
         <Text style={styles.TextStyle}> Login Using Github </Text>
 
       </TouchableOpacity>
+
+      <Button
+      disabled={!request}
+      title="Login"
+      onPress={() => {
+        promptAsync();
+        }}
+    />
       <Button title="Home Page" onPress={() => history.push("/home")}/>
     </View> 
   );
